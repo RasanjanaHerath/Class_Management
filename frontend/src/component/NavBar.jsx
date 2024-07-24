4dd1c916f3421ab4cddaa22e51bd61f409b99648
kanishkadharmarathne <kanishkadewinda1102@gmail.com>
2024-07-25 00:29:12 +0530

    FEAT:modify the navigation bar

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -5,22 +5,18 @@ const NavBar = () => {
   return (
     <div>
       <div>
-      <nav className="bg-white shadow-md">
+      <nav className="bg-white shadow-md px-24 py-3.5">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="flex items-center">
-          <img src={logo} alt="Logo" className="h-8 w-8 mr-2" />
-          <span className="font-bold text-lg text-gray-800">ClassMaster</span>
+          <img src={logo} alt="Logo" className="h-8 w-24 mr-4" />
+          <span className="font-bold text-2xl text-blue-800 ">Class Master</span>
         </div>
-        <div className="flex space-x-6">
-          <a href="#demos" className="text-gray-800 hover:text-blue-500">HOME</a>
-          <a href="#features" className="text-gray-800 hover:text-blue-500">ABOUT</a>
-          <a href="#pages" className="text-gray-800 hover:text-blue-500">CONTACT US</a>
+        <div className="flex space-x-6 ">
+          <a href="#home" className="text-gray-800 hover:text-blue-500 pr-11">HOME</a>
+          <a href="#about" className="text-gray-800 hover:text-blue-500 pr-11">ABOUT</a>
+          <a href="#contact us" className="text-gray-800 hover:text-blue-500 pr-11">CONTACT US</a>
         </div>
-        <div className="flex items-center space-x-4">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-            SIGN IN
-          </button>
-        </div>
+        
       </div>
     </nav>
     </div>

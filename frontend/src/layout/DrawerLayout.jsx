9be0ccb86af3280c847c2ebade760d928ef66818
MadhawaPrabhath <madhawaprabhathd@gmail.com>
2024-09-27 02:42:42 +0530

    develop SuperAdminDashboard

--- frontend/src/layout/DrawerLayout.jsx
+++ frontend/src/layout/DrawerLayout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Drawer from '../component/Drawer';
+import Header from '../component/Header';
 
 
 const DrawerLayout = () => {
   return (
     <div>
-        
+
         <Drawer/>
+        <Header />
         <Outlet/>
         
     </div>

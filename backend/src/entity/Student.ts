37a06fcc88c61ef9a4ecabe682dec3b80e3de0df
ChandeepaAbhisheka <chandeepaabhisheka455@gmail.com>
2024-10-29 11:17:22 +0530

    develop student table

--- backend/src/entity/Student.ts
+++ backend/src/entity/Student.ts
@@ -1,27 +1,26 @@
-// src/entity/Student.ts
-// import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
-// import { Class } from './Class';
-// import { Institute } from './Institute';
-// import { Teacher } from './Teacher';
-// import { Notice } from './Notice';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
+import { Class } from './Class';
+import { Institute } from './Institute';
+import { Teacher } from './Teacher';
+import { Notice } from './Notice';
 
-// @Entity('students')
-// export class Student {
-//   @PrimaryGeneratedColumn('uuid')
-//   id: string;
+@Entity('students')
+export class Student {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-//   @Column()
-//   name: string;
+  @Column()
+  name: string;
 
-//   @ManyToOne(() => Class, (classEntity) => classEntity.students)
-//   class: Class;
+  @ManyToOne(() => Class, (classEntity) => classEntity.students)
+  class: Class;
 
-//   @ManyToOne(() => Institute, (institute) => institute.students)
-//   institute: Institute;
+  @ManyToOne(() => Institute, (institute) => institute.students)
+  institute: Institute;
 
-//   @ManyToOne(() => Teacher, (teacher) => teacher.students)
-//   teacher: Teacher;
+  @ManyToOne(() => Teacher, (teacher) => teacher.students)
+  teacher: Teacher;
 
-//   @OneToMany(() => Notice, (notice) => notice.student)
-//   notices: Notice[];
-// }
+  @OneToMany(() => Notice, (notice) => notice.student)
+  notices: Notice[];
+}

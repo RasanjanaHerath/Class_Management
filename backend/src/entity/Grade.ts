08e8b26137aeb76f3533400d77cd4c01e3762679
ChandeepaAbhisheka <chandeepaabhisheka455@gmail.com>
2024-10-29 11:36:18 +0530

    develop grade table

--- backend/src/entity/Grade.ts
+++ backend/src/entity/Grade.ts
@@ -1,18 +1,17 @@
-// // src/entity/Grade.ts
-// import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-// import { Institute } from './Institute';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Institute } from './Institute';
 
-// @Entity('grades')
-// export class Grade {
-//   @PrimaryGeneratedColumn('uuid')
-//   id: string;
+@Entity('grades')
+export class Grade {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-//   @Column()
-//   name: string;
+  @Column()
+  name: string;
 
-//   @Column()
-//   value: string;
+  @Column()
+  value: string;
 
-//   @ManyToOne(() => Institute, (institute) => institute.grades)
-//   institute: Institute;
-// }
+  @ManyToOne(() => Institute, (institute) => institute.grades)
+  institute: Institute;
+}

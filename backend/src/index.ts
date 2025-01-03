572c4753e4e8d396a13996862c094845c4e24615
RasanjanaHerath <rasanjanaherath1999@gmail.com>
2025-01-03 20:21:46 +0530

    backend studnet profile complteed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,7 @@ import cors from 'cors';
 import notice_router from "./route/notice.routes";
 require('dotenv').config();
 import classRoutes from "./route/classRoutes"; 
-<<<<<<< HEAD
 import teacherRoutes from "./route/TeacherRouts";
-=======
-import enrollClass_router from "./route/enrollclass.routes";
->>>>>>> 45b766a (FEAT: Create a class page)
 
 
 AppDataSource.initialize().then(async () => {
@@ -29,11 +25,7 @@ AppDataSource.initialize().then(async () => {
     app.use("/api", instituteRoutes);
     app.use("/api", notice_router);
     app.use("/api",classRoutes);
-<<<<<<< HEAD
     app.use("/api", teacherRoutes)
-=======
-    app.use("/api",enrollClass_router);
->>>>>>> 45b766a (FEAT: Create a class page)
 
     app.listen(3000, () => {
         console.log("Server is running on port 3000");

7225b55fba95bf04940efb146623fe78e7bf76bd
ChandeepaAbhisheka <chandeepaabhisheka455@gmail.com>
2025-01-16 19:55:59 +0530

    change form

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -246,18 +246,6 @@ const SignIn = () => {
             {errors.lastName && <p className="text-red-500 text-sm">{errors.lastName}</p>}
           </div>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-          </div>
-          <div className="mb-4">
             <label htmlFor="userName" className="block text-gray-700">Username</label>
             <input
               type="text"
@@ -270,6 +258,19 @@ const SignIn = () => {
             {errors.userName && <p className="text-red-500 text-sm">{errors.userName}</p>}
           </div>
           <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+          </div>
+          
+          <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700">Password</label>
             <input
               type="password"
@@ -280,6 +281,22 @@ const SignIn = () => {
               className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700">Role</label>
+            <select
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
+              className="w-full px-3 py-2 mt-1 border rounded"
+            >
+              <option value="">Select Role</option>
+              <option value="student">Student</option>
+              <option value="teacher">Teacher</option>
+              <option value="institute">Institute</option>
+            </select>
+            {errors.role && <p className="text-red-500 text-sm">{errors.role}</p>}
+            
           </div>
           {errors.form && <p className="text-red-500 text-sm mb-4">{errors.form}</p>}
           <button

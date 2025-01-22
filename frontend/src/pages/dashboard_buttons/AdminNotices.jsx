fcff9b3141e6426b3eaadb5c76d687e46821b770
MadhawaPrabhath <madhawaprabhathd@gmail.com>
2025-01-23 04:08:50 +0530

    update forms

--- frontend/src/pages/dashboard_buttons/AdminNotices.jsx
+++ frontend/src/pages/dashboard_buttons/AdminNotices.jsx
@@ -84,7 +84,7 @@ const AdminNotices = () => {
     <div className="min-h-screen bg-gray-50 to-pink-100 p-10 md:ml-64">
       <div className="w-full max-w-8xl bg-white rounded-xl shadow-2xl p-10">
         <div className="flex justify-between items-center mb-8">
-          <h1 className="text-3xl font-bold text-gray-800">Admin Notices</h1>
+          <h1 className="text-2xl font-bold text-gray-800">Admin Notices :</h1>
           <button
             className="bg-gradient-to-r from-blue-500 to-purple-500 text-white px-6 py-3 rounded-lg hover:shadow-lg transform hover:scale-105 transition duration-300"
             onClick={() => openModal()}
@@ -99,7 +99,7 @@ const AdminNotices = () => {
               key={notice.id}
               className="p-6 rounded-xl shadow-lg bg-white transition transform hover:scale-105 hover:shadow-xl"
             >
-              <h3 className="text-xl font-bold text-blue-8 mb-2 text-center">
+              <h3 className="text-xl font-bold text-gray-600 mb-2 text-center">
                 {notice.title}
               </h3>
               <p className="text-sm text-gray-500 italic font-bold">Role: {notice.role}</p>
